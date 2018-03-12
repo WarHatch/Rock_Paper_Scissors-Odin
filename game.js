4e891e90dba07fed767a56627a7a4c56eb50d7e6
WarHatch <staskeviciuskarolis@gmail.com>
2018-03-12 22:05:10 +0200

    Webpage displays outcome and score

--- game.js
+++ game.js
@@ -4,69 +4,84 @@ function computerPlay(){
 }
 
 function playRound(e){
-    let computerSelection = computerPlay();
-    let playerSelection = e.target.innerHTML;
-    console.log("Round played");
+    if (compScore < 5 && playerScore < 5)
+    {
+        let computerSelection = computerPlay();
+        let playerSelection = e.target.innerHTML;
+        console.log("Round played");
 
-    if (playerSelection.toUpperCase() === computerSelection.toUpperCase()){
-        console.log("Tie. You both selected " + computerSelection);
-        updateScore(0);
-        return 0; 
-    }
+        if (playerSelection.toUpperCase() === computerSelection.toUpperCase()){
+            outcomeDiv.textContent = "Tie. You both selected " + computerSelection;
+            updateScore(0);
+            return 0; 
+        }
 
-    else if (playerSelection.toUpperCase() === "ROCK"){
-        if (computerSelection === "Paper"){
-            console.log("You Lose! Rock loses to Paper");
-            updateScore(-1);
-            return -1; 
+        else if (playerSelection.toUpperCase() === "ROCK"){
+            if (computerSelection === "Paper"){
+                outcomeDiv.textContent = "You Lose! Rock loses to Paper";
+                updateScore(-1);
+                return -1; 
+            }
+            else{
+                outcomeDiv.textContent = "You Win! Rock beats Scissors";
+                updateScore(1);
+                return 1; 
+            }
         }
-        else{
-            console.log("You Win! Rock beats Scissors");
-            updateScore(1);
-            return 1; 
-        }
-    }
 
-    else if (playerSelection.toUpperCase() === "PAPER"){
-        if (computerSelection === "Scissors"){
-            console.log("You Lose! Paper loses to Scissors");
-            updateScore(-1);
-            return -1; 
+        else if (playerSelection.toUpperCase() === "PAPER"){
+            if (computerSelection === "Scissors"){
+                outcomeDiv.textContent = "You Lose! Paper loses to Scissors";
+                updateScore(-1);
+                return -1; 
+            }
+            else{
+                outcomeDiv.textContent = "You Win! Paper beats Rock";
+                updateScore(1);
+                return 1; 
+            }
         }
-        else{
-            console.log("You Win! Paper beats Rock");
-            updateScore(1);
-            return 1; 
-        }
-    }
 
-    else if (playerSelection.toUpperCase() === "SCISSORS"){
-        if (computerSelection === "Rock"){ 
-            console.log("You Lose! Scissors lose to Rock");
-            updateScore(-1);
-            return -1; 
-        }
-        else{
-            console.log("You Win! Scissors beat Paper");
-            updateScore(1);
-            return 1; 
+        else if (playerSelection.toUpperCase() === "SCISSORS"){
+            if (computerSelection === "Rock"){ 
+                outcomeDiv.textContent = "You Lose! Scissors lose to Rock";
+                updateScore(-1);
+                return -1; 
+            }
+            else{
+                outcomeDiv.textContent = "You Win! Scissors beat Paper";
+                updateScore(1);
+                return 1; 
         }
     }
 
     console.log("ERROR: Did not recognize the move");
-    return 0;   
+    return 0;
+    }   
 }
 
 function updateScore(roundOutcome)
 {  
+    scoreContainer = document.querySelector("#score");
+
     roundOutcome === 1 ? playerScore++ : roundOutcome === -1 ? compScore++ : 0;
-    console.log("Player's score: " + playerScore + "\n"
-        + "Computer's score: " + compScore + "\n"
-        + "The winner is " + (playerScore > compScore? "Player" : playerScore < compScore? "Computer" : "No one") +"!");
+
+    scoreContainer.textContent = "Player's score: " + playerScore + "\n"
+    + "Computer's score: " + compScore + "\n";
+
+    if (playerScore >= 5)
+        scoreContainer.innerHTML += "<br/>Players wins!";
+    else if (compScore >= 5)
+        scoreContainer.innerHTML += "<br/>Computer wins";
 }
 
 var playerScore = 0;
 var compScore = 0;
+
+const resultsContainer = document.querySelector("#results");
+const outcomeDiv = document.createElement('div');
+outcomeDiv.textContent = '';
+resultsContainer.appendChild(outcomeDiv);
 
 const choiceButtons = Array.from( document.querySelectorAll('.Choice'));
 choiceButtons.forEach( button => button.addEventListener( 'click', playRound ) );
